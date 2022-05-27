36b68bccf7870fe6f24ee5f8b38ff46f3e55aa67
“tarunve” <tarun.verma151@gmail.com>
2022-05-27 11:36:07 +0530

    adding unit test

--- spring-boot-custom-rate-limiter-rest/src/test/java/com/spring/boot/rest/SpringBootCustomRateLimiterRestApplicationTests.java
+++ spring-boot-custom-rate-limiter-rest/src/test/java/com/spring/boot/rest/SpringBootCustomRateLimiterRestApplicationTests.java
@@ -1,13 +1,26 @@
 package com.spring.boot.rest;
 
+import com.spring.boot.rest.controller.DeveloperController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringBootCustomRateLimiterRestApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SpringBootCustomRateLimiterRestApplicationTests {
+
+    @Autowired
+    private DeveloperController developerController;
 
     @Test
     void contextLoads() {
+        Assertions.assertThat(developerController).isNotNull();
+    }
+
+    @Test
+    public void testMainMethod() {
+        SpringBootCustomRateLimiterRestApplication.main(new String[]{});
     }
 
 }
