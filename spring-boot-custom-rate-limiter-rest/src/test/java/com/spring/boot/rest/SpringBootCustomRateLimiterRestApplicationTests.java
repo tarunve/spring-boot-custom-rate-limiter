27879d941d453be8bf6121bb88e13fcffe80e6ad
“tarunve” <tarun.verma151@gmail.com>
2022-05-28 00:29:57 +0530

    updating unit tests

--- spring-boot-custom-rate-limiter-rest/src/test/java/com/spring/boot/rest/SpringBootCustomRateLimiterRestApplicationTests.java
+++ spring-boot-custom-rate-limiter-rest/src/test/java/com/spring/boot/rest/SpringBootCustomRateLimiterRestApplicationTests.java
@@ -4,7 +4,6 @@ import com.spring.boot.rest.controller.DeveloperController;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
